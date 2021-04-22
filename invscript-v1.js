200cf8eae755aa533b2fcd0f874bced154fa56ca
Kyyros04 <81814164+Kyyros04@users.noreply.github.com>
2021-04-22 17:10:19 +0200

    Add files via upload

--- invscript-v1.js
+++ invscript-v1.js
@@ -1,3 +1,5 @@
+var arraytemp=[];
+
 class CreaItem{
   constructor(nome,qnt=1,prz=0){
 	this.nome=nome;
@@ -19,6 +21,11 @@ function puoiinviare(){
 		document.getElementById("accendiamo").onclick=aggiungiitem
 	}	
 }
+/*attivatore di tasto riordina, per ora non serve
+document.getElementById("tabitem").addEventListener("change", puoiriordinare)
+function puoiriordinare(){
+	document.getElementById("accendiamo1").onclick=riordina		
+}*/
 
 function add(id,nome,qnt){
 	do{
@@ -43,10 +50,18 @@ function rem(id,nome,qnt){
 	if(item.qnt==0){
 		var dimenticato=prompt("vuoi dimenticarti di tale oggetto?");
 		if(dimenticato=="si"){
+			arraytemp.push(id.match(/[0-9]+/g)[0]);
 			var parent=target.parentNode;
 			parent.parentNode.removeChild(parent);
 		}
 	}
+}
+
+function oss(imm){
+	var cod=imm;
+	var url= `<img src="Cards/${imm.toString()}.png" alt="Devi aver dimenticato cosa stavi cercando" height="280px">`
+	var target=document.getElementById("display");
+	target.innerHTML=url
 }
 
 function aggiungiitem(){
@@ -61,13 +76,57 @@ function aggiungiitem(){
 	while(!(prz>=0))
 	var item= new CreaItem(nom.value.toString(),qnt,prz)
 	var padre=document.getElementById("tabitem");
-	var n=padre.getElementsByTagName("tr").length;
+	var n;
+	if(arraytemp==0)
+		 n=padre.getElementsByTagName("tr").length;
+	else
+		{
+			n=arraytemp[0];
+			arraytemp.shift();
+		}
 	var nodino=document.createElement("tr");
-	nodino.id=n.toString();
-	var riga=`<td id="n${n.toString()}">${item.nome}</td><td id="q${n.toString()}">${item.qnt}</td><td id="p${n.toString()}">	${item.prz}</td><td><input type='button' value='+' onclick='add("q${n.toString()}","${item.nome}",document.getElementById("q${n.toString()}").innerHTML)'></button></td><td><input type='button' 	value='-' onclick='rem("q${n.toString()}","${item.nome}",document.getElementById("q${n.toString()}").innerHTML)'></button></td><td><input type='button' value='O_O' 	onclick='oss()'></button></td></tr>`
+	nodino.style.display="table-row";
+	nodino.id=n.toString()
+	var riga=`<td id="n${n.toString()}" onclick='oss("${item.nome}")'>${item.nome}</td><td id="q${n.toString()}">${item.qnt}</td><td id="p${n.toString()}">${item.prz}</td><td><input type='button' value='+' onclick='add("q	${n.toString()}","${item.nome}",document.getElementById("q${n.toString()}").innerHTML)'></button></td><td><input type='button' 	value='-' onclick='rem("q${n.toString()}","${item.nome}",document.getElementById("q${n.toString	()}").innerHTML)'></button></td><td><select name="Categoria" id="categ${n.toString()}"><option value="All" selected="selected">All</option><option value="Armi">Armi</option><option value="Scudi">Scudi</option><option value="Elmi">Elmi</option><option value="Armor">Armor</option><option value="Oggetti">Oggetti</option><option value="Altro">Altro</option></select></td></tr>`
 	padre.appendChild(nodino).innerHTML=riga
 	nom.value="";
 	document.getElementById("accendiamo").onclick="";
+	//alert(padre.getElementsByTagName("tr")[0].innerHTML.match(/[\w +-]+(?=<)/g)[0])
+}
+
+function riordina(){
+	var padre=document.getElementById("tabitem");
+	var n=padre.getElementsByTagName("tr").length;
+	if(n==0)return;
+	var arr2=padre.getElementsByTagName("tr");
+	var arr1=[];
+	//var temp;
+	for(var i=0;i<n;i++){
+   		arr1.push([padre.getElementsByTagName("tr")[i].innerHTML.match(/[\w +-]+(?=<)/g)[0],padre.getElementsByTagName("tr")[i].innerHTML.match(/[0-9]+/g)[0]]);
+	}
+	//fin qui ok
+	arr1.sort();
+	//alert("in fase di lavorazione"+arr1.toString());
+	for(var j=0;j<n;j++){
+	  padre.insertBefore(document.getElementById(arr1[j][1].toString()),arr2[j]);
+	}
 }
 	
+function vis(parametro){
+	var padre=document.getElementById("tabitem");
+	var arr=padre.getElementsByTagName("tr");
+	var n=arr.length;
+	var ident;
+	var categ;
+	for(var i=0;i<n;i++){
+	 ident=arr[i].id;
+	 categ="categ"+ident
+	 //alert(ident+categ+parametro+arr[i].toString());
+	 if(parametro=="All") document.getElementById(ident).style.display="table-row";
+	 else{
+	    document.getElementById(ident).style.display="table-row";
+	    if(document.getElementById(categ).value!=parametro) document.getElementById(ident).style.display="none";
+	  }
+	}
+}
 
